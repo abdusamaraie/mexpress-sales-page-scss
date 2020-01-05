4c887173a99ba111b34fa547d7aa550e02d7ebbb
Abdu Samaraie <abdusamaraie@gmail.com>
2020-01-04 19:20:13 -0500

    add footer section and smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 // Get the current year for the copyright
 $("#year").text(new Date().getFullYear());
+
+//init scroll spy
+
+$("body").scrollspy({
+  target: "#main-nav"
+});
+
+//smooth scrolling
+
+$('#main-nav a').on('click',function(event){
+    if(this.hash !== ''){
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollspy: $(hash).offset().top;
+        },800,function(){
+            window.location.hash = hash;
+        })
+        
+    }
+})
